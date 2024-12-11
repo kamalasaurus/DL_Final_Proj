554358e378a754b8960017dd2187a054408093ba
Sam <samuel.moerman@gmail.com>
2024-12-11 18:28:23 -0500

    update

--- anotherJEPA3.py
+++ anotherJEPA3.py
@@ -84,7 +84,7 @@ def flip_and_shift_augmentation(states, actions):
     # Randomly determine shift (without breaking out of the box)
     min_shift = 5 - global_min_all
     max_shift = 59 - global_max_all
-    if min_shift is not max_shift:
+    if min_shift is not max_shift+1 or min_shift is not max_shift:
         shift = torch.randint(min_shift, max_shift + 1, size=(1,))
     else:
         shift = min_shift
