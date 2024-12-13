bcd56cf26b0c1815915950ec4b6fe9c1a8537b72
Samuel Moerman <shm9853@b-19-41.c.hpc-slurm-9c75.internal>
2024-12-13 12:31:07 -0500

    vit b64 e10 flip shift

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def load_model():
         else 'mps' if torch.backends.mps.is_available()
         else 'cpu'
     )
-    state_dict_path = "trained_jepa_vit.pth"
+    state_dict_path = "trained_jepa_vit_flip_shift.pth"
 
     state_dim = 128
     action_dim = 2
